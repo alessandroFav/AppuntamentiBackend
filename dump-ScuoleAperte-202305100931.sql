f72973ba4a34ed76524450a079e2992c3c8b5fbc
Scala28 <scaluca04@gmail.com>
2023-05-16 11:12:05 +0200

    Update dump SQL

--- dump-ScuoleAperte-202305100931.sql
+++ dump-ScuoleAperte-202305100931.sql
@@ -1,28 +1,24 @@
--- MySQL dump 10.13  Distrib 5.7.34, for osx11.0 (x86_64)
---
--- Host: localhost    Database: ScuoleAperte
--- ------------------------------------------------------
--- Server version	5.7.34
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versione server:              10.4.21-MariaDB - mariadb.org binary distribution
+-- S.O. server:                  Win64
+-- HeidiSQL Versione:            11.3.0.6295
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `appuntamenti`
---
 
-DROP TABLE IF EXISTS `appuntamenti`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `appuntamenti` (
+-- Dump della struttura del database scuoleaperte
+CREATE DATABASE IF NOT EXISTS `scuoleaperte` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `scuoleaperte`;
+
+-- Dump della struttura di tabella scuoleaperte.appuntamenti
+CREATE TABLE IF NOT EXISTS `appuntamenti` (
   `id` float NOT NULL AUTO_INCREMENT,
   `idEvento` int(11) DEFAULT NULL,
   `idUtente` int(11) DEFAULT NULL,
@@ -33,48 +29,23 @@ CREATE TABLE `appuntamenti` (
   CONSTRAINT `appuntamenti_FK` FOREIGN KEY (`idEvento`) REFERENCES `eventi` (`id`),
   CONSTRAINT `appuntamenti_FK_1` FOREIGN KEY (`idUtente`) REFERENCES `utenti` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `appuntamenti`
---
-
-LOCK TABLES `appuntamenti` WRITE;
+-- Dump dei dati della tabella scuoleaperte.appuntamenti: ~2 rows (circa)
 /*!40000 ALTER TABLE `appuntamenti` DISABLE KEYS */;
-INSERT INTO `appuntamenti` VALUES (1,1,1,NULL),(5,1,1,'0001-01-01 00:00:00');
 /*!40000 ALTER TABLE `appuntamenti` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `calendario`
---
-
-DROP TABLE IF EXISTS `calendario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `calendario` (
+-- Dump della struttura di tabella scuoleaperte.calendario
+CREATE TABLE IF NOT EXISTS `calendario` (
   `data` datetime NOT NULL,
   PRIMARY KEY (`data`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `calendario`
---
-
-LOCK TABLES `calendario` WRITE;
+-- Dump dei dati della tabella scuoleaperte.calendario: ~0 rows (circa)
 /*!40000 ALTER TABLE `calendario` DISABLE KEYS */;
 /*!40000 ALTER TABLE `calendario` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `eventi`
---
-
-DROP TABLE IF EXISTS `eventi`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `eventi` (
+-- Dump della struttura di tabella scuoleaperte.eventi
+CREATE TABLE IF NOT EXISTS `eventi` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) DEFAULT NULL,
   `materia` varchar(100) DEFAULT NULL,
@@ -87,57 +58,31 @@ CREATE TABLE `eventi` (
   KEY `eventi_FK` (`idOrganizzatore`),
   CONSTRAINT `eventi_FK` FOREIGN KEY (`idOrganizzatore`) REFERENCES `utenti` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `eventi`
---
-
-LOCK TABLES `eventi` WRITE;
+-- Dump dei dati della tabella scuoleaperte.eventi: ~0 rows (circa)
 /*!40000 ALTER TABLE `eventi` DISABLE KEYS */;
-INSERT INTO `eventi` VALUES (1,'Tecnica','Tecnica',NULL,1,20,30,NULL);
 /*!40000 ALTER TABLE `eventi` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `utenti`
---
-
-DROP TABLE IF EXISTS `utenti`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `utenti` (
+-- Dump della struttura di tabella scuoleaperte.utenti
+CREATE TABLE IF NOT EXISTS `utenti` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) DEFAULT NULL,
-  `cognome` varchar(100) DEFAULT NULL,
-  `mail` varchar(100) DEFAULT NULL,
-  `password` varchar(100) DEFAULT NULL,
-  `ruolo` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `nome` varchar(100) NOT NULL,
+  `cognome` varchar(100) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `VerifiedAt` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `mail` (`mail`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `utenti`
---
-
-LOCK TABLES `utenti` WRITE;
+-- Dump dei dati della tabella scuoleaperte.utenti: ~2 rows (circa)
 /*!40000 ALTER TABLE `utenti` DISABLE KEYS */;
-INSERT INTO `utenti` VALUES (1,'Alessandro','Favaro','ale','ale','admin');
+INSERT INTO `utenti` (`id`, `nome`, `cognome`, `mail`, `password`, `VerifiedAt`) VALUES
+	(23, 'luca', 'scala', 'lucascala@blabla', '$2a$11$08M91.016zlKr3s5BL67UOgzsvQNKehEtBBDXCnGOZlAY5PXGpg8.', '2023-05-15'),
+	(24, 'ale', 'dina', 'aledina@blabla', '$2a$11$e3pBJgo5vWQ8eurKpAACEeHpiu1EfO/IyKZ0Zm13uI/g8wn41Zz1y', '2023-05-15');
 /*!40000 ALTER TABLE `utenti` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Dumping routines for database 'ScuoleAperte'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-05-10  9:31:57
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
